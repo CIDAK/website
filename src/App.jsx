417e6dfb8b7e0013f53201b332c9332956eba050
Trckmazz <dylanprinsloo@icloud.com>
2025-08-05 22:16:23 +0200

    sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -3,21 +3,31 @@ import './App.css';
 function App() {
   return (
     <div className="app-layout">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="left-panel">
+        <div className="content">
+          <p className="description">
+            CIDAK is <span className="custom-underline" onClick={() => window.open('https://london.ac.uk', '_blank')}>University of London</span> Computer Science<br />
+            society.
+          </p>
+          <p className="description">
+            Our focus is to participate and<br />
+            win various hackathons of all caliber.
+
+          </p>
+          <p className="contact">
+            Dylan, Ian, Alex, Ken and Chey<br />
+            &gt; <a href="mailto:contact@cidak.co?subject=Hello from CIDAK website" className="custom-underline">contact@cidak.co</a>
+          </p>
+        </div>
+      </div>
+      
+      <div className="vertical-divider"></div>
+      <div className="vertical-divider-2"></div>
+
+      <div className="right-panel">
+      </div>
     </div>
   );
-}
+};
 
 export default App;
