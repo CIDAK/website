25b40f52d606e57bed5e6b42631c54df199a7fba
Dylan.Prinsloo <dylanprinsloo@icloud.com>
2025-08-09 19:35:50 +0200

    cidak problem set

--- public/static/problem-set-download/cidak.js
+++ public/static/problem-set-download/cidak.js
@@ -3,15 +3,11 @@
 
 
 /* 
-COVID-19 taught the world the importance of prepardeness. In a Harvard blog, Dr. Tedros Adhanom Ghebreyesus, director-general of the WHO, outlined the lessons 
-the world should take to heart. The first: science has to guide policy. The politicization of the pandemic led to a lot of unnecessary damage. Another lesson 
-is that science must pair with equity or it can actually make inequalities worse. This is obvious when looking at how low-income countries struggled to get 
-the vaccines while wealthier countries stocked up. More resilient healthcare systems are also a must, as well as more coherent, global plans on how to respond. 
-The world must also invest in research on contagious diseases, zoonotic diseases, the effectiveness of outbreak responses, and more.
+
 
 This problem you will work on is as follows:
 
-You need to take the following publicly avaliable dataset here => 
+
 
 
 
